14b95da4379520f36be2e69438df36e0f0687962
unknown <naim.ahmed035@gmail.com>
2021-08-02 22:43:43 +0600

    Connect with MongoDB

---
+++ server/src/mongo.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+require('dotenv').config();
+
+const uri = `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
+const options = { useNewUrlParser: true, useUnifiedTopology: true };
+
+const connectWithDb = () => {
+    mongoose.connect(uri, options, (err) => {
+        if (err) {
+            console.error(err);
+        } else {
+            console.log("Database connection has been established");
+        }
+    });
+};
+
+module.exports = connectWithDb;
--- server/src/server.js
+++ server/src/server.js
@@ -1,8 +1,11 @@
-const express = require("express");
+const logger = require('pino')();
+const app = require('./app');
 
-const app = express();
+const connectWithDb = require("./mongo");
 
 const PORT = 5000;
 app.listen(PORT, () => {
+    connectWithDb();
     console.log(`Server is running on port: http://localhost::${PORT}`);
+    logger.info(`Server is running on port: http://localhost::${PORT}`);
 });
