3c7e601f158a319c4ae4c3534108982f32ea4532
unknown <naim.ahmed035@gmail.com>
2021-08-02 23:57:09 +0600

    Use necessary packages

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,22 @@
 const express = require("express");
+const compression = require('compression')
+const cors = require("cors");
+const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
+const pino = require('pino-http')();
+require('dotenv').config()
+
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 1000 // limit each IP to 1000 requests per windowMs
+});
 
 const app = express();
+app.use(compression());
+app.use(cors());
+app.use(limiter);
+app.use(express.json());
+app.use(helmet());
+app.use(pino);
 
 module.exports = app;
